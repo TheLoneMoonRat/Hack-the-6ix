dd08e29ef8149978dd62637fbfea87075fe87fd3
Nathan <nhnwong@gmail.com>
2022-08-20 20:07:13 -0400

    Sorting out projectiles and player interaction

--- HeliCapture/Assets/scripts/camMovement.cs
+++ HeliCapture/Assets/scripts/camMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class camMovement : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +13,8 @@ public class camMovement : MonoBehaviour
     {
         Vector3 mouseMovement = Input.mousePosition;
         float sideMotion = Input.GetAxisRaw("Horizontal");
-        transform.Translate(sideMotion/90, 0.02f, 0);
+        float forwardMotion = Input.GetAxisRaw("Vertical");
+        transform.Translate(sideMotion/90, forwardMotion/90 + 0.02f, 0);
         if (mouseMovement.x > 1664) {
             transform.Rotate(0, 0, -0.1f);
         } else if (mouseMovement.x < 896) {
